439a00fa6535e4bac6bdc8fb6e2a7c32682da6b0
Thisali.Jayasundara <user@example.comthisalirj@gmail.com>
2026-03-08 13:08:14 +0530

    Updated python program to finalized the program

--- program.py
+++ program.py
@@ -1,2 +1,6 @@
 #Python program to check if the input number is odd or even
 number=int(input("Enter your number : "))
+if (number % 2)== 0:
+    print("Your entered number is an even number")
+else :
+    print("Your entered number is an odd number")
