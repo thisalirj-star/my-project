ae5d51c693dc89d596896e409a8bec4889d84f3b
Thisali.Jayasundara <user@example.comthisalirj@gmail.com>
2026-03-08 16:53:10 +0530

    Finalized the  branch3program python file

--- branch3 program.py
+++ branch3 program.py
@@ -1,1 +1,5 @@
 #Multiplying two numbers
+number_1=int(input("Enter your first number :"))
+number_2=int(input("Enter your second number :"))
+answer=number_1*number_2
+print("Your answer is {answer}")
