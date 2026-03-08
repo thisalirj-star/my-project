30c8ac4f038412d1aa4ac795f9f80ac2ed127347
Thisali.Jayasundara <user@example.comthisalirj@gmail.com>
2026-03-08 13:42:44 +0530

    Finalized the  branch1 program python file

--- branch1 program.py
+++ branch1 program.py
@@ -1,1 +1,8 @@
 #Finding out the larger number from the entered two numbers
+num1=int(input("Enter your first number :"))
+num2=int(input("Enter your second number"))
+if num1>num2 :
+    print("{num1}is the largest number.")
+else :
+    print("{num2} is the largest number.")
+    
